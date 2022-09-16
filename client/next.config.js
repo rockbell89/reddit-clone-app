7844d2d339ca152ed061aab1b25cc29032f9f040
BRWEB <52031484+rockbell89@users.noreply.github.com>
2022-09-16 14:49:28 +0900

    fixed domains

--- client/next.config.js
+++ client/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     domains: [
       "www.gravatar.com",
       "localhost",
-      "ec2-54-180-31-7.ap-northeast-2.compute.amazonaws.com",
+      "ec2-3-36-90-118.ap-northeast-2.compute.amazonaws.com",
     ],
   },
 };
